38f63830cfcfb22ba05add5633e9d6868ec5741a
paulQR <paulqr10@gmail.com>
2019-07-18 16:24:32 +0200

    1 file modified

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,10 @@ import{HttpClient} from '@angular/common/http';
   templateUrl: './home.component.html', 
 })
 export class HomeComponent implements OnInit {
-    constructor( private http: HttpClient ){}
+    constructor( private http: HttpClient ){
+        console.log('Constructor del home hecho')
+        this.http.get('https://restcountries.eu/rest/v2/lang/es');
+    }
 
     ngOnInit(){
 
