4980e97ff21f1cd32ca9d7787a27dd12134e1a15
paulQR <paulqr10@gmail.com>
2019-07-18 16:07:39 +0200

    6 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+//<--- Modulo de rutas
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
@@ -10,8 +13,12 @@ import { ArtistaComponent } from './components/artista/artista.component';
 import { SearchComponent } from './components/search/search.component';
 
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+// importar rutas
+import { ROUTES } from './app.routes';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(ROUTES, {useHash:true}) ],
   declarations: [ AppComponent, HelloComponent, HomeComponent, ArtistaComponent, SearchComponent, NavbarComponent ],
   bootstrap:    [ AppComponent ]
 })
